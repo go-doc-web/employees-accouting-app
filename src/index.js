5e3fe3d44f9dd575daef5d8f69741a2074199292
Oleg Gubskiy <gubskyolegod@gmail.com>
2023-01-08 20:39:21 +0200

    Разворачивает Реакт

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// const elem = <h2>Hello World</h2>;
+
+const text = "Hello React";
+const elem = (
+  <div>
+    <h2 className={text}>Text:{text}</h2>
+    <input type="text" />
+    <button>Click</button>
+  </div>
 );
+console.log(elem);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(elem);
