e188d58b4f02fc4fe523a8c834543e7ad06454a0
Oleg Gubskiy <gubskyolegod@gmail.com>
2023-01-16 14:51:10 +0200

    некотрые незначительные правки

--- src/componets/employees-list/employees-list.js
+++ src/componets/employees-list/employees-list.js
@@ -2,8 +2,8 @@ import EmployeesListItem from '../employees-list-item/employees-list-item';
 import './employees-list.css';
 
 function EmployeesList({ items }) {
-  const elements = items.map(({ id, ...props }) => (
-    <EmployeesListItem key={id} {...props} />
+  const elements = items.map(({ id, ...itemProps }) => (
+    <EmployeesListItem key={id} {...itemProps} />
   ));
   console.log(items);
   return <ul className="app-list list-group">{elements}</ul>;
